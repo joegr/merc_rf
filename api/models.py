f07f224f0925dde72931bf9b6c78d6c2545d4812
joegr <jpg88@georgetown.edu>
2020-05-01 11:29:46 -0500

    first commit

---
+++ api/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+# We're weorking with the default User setup, so we won't need any additional models in the database.
+# To allow for cascading delete, we can set delete mode of foreign models to Models.CASCADE
